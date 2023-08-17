8e7c050f777699bb244c60d275f2907e6cfc0bd3
ihsajjad <ihsajjad1@gmail.com>
2023-08-17 13:51:08 +0600

    created notfound page for unmatched routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,11 +3,13 @@ import Main from "../layouts/Main";
 import Home from "../pages/home/home/Home";
 import Register from "../pages/register/Register";
 import Login from "../pages/login/Login";
+import NotFound from "../pages/404/NotFound";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "/",
